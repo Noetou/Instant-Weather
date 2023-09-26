2f7acc69595d068a60b0b79ae5fa321d45ce7ef6
Noe Tourbillon <noe.tourbillon@etu.unicaen.fr>
2023-09-26 10:02:33 +0200

    feat : checkboxes to choose the informations the user wants to see

--- js/script.js
+++ js/script.js
@@ -32,10 +32,10 @@ pc_input.addEventListener('input', () => {
                         inputError.textContent = "The post code entered is unknown";
                         return;
                     }
-                    if(!document.querySelectorAll("select").length==0){
+                    if (!document.querySelectorAll("select").length == 0) {
                         document.querySelector("select").remove();
                     }
-                    if (data.length > 1 ) {
+                    if (data.length > 1) {
                         const selectInput = document.createElement("select")
                         for (let index = 0; index < data.length; index++) {
                             let option = document.createElement("option")
@@ -63,39 +63,127 @@ pc_submit.addEventListener('click', () => {
     pc_input.disabled = true;
 
     fetch('https://geo.api.gouv.fr/communes?codePostal='.concat(pc))
-    .then(response => response.json())
-    .then(data => {
-        try{
-            codeInsee = data[cityN]['code'];
-        }
-        catch{
-            inputError.textContent="The post code entered is unknown";
-            return;
-        }
-        fetch(`https://api.meteo-concept.com/api/forecast/daily?token=9fc5110929e9db4b61fcc700441c5d39e82c9e6d6aeeacc3223498621f238c38&insee=${codeInsee}`)
         .then(response => response.json())
         .then(data => {
-            let weatherTmin = document.createElement('div');
-            let weatherTmax = document.createElement('div');
-            let weatherPrain = document.createElement('div');
-            let weatherSunHours = document.createElement('div');
-            // Ajouter du contenu aux divs
-            weatherTmin.innerHTML = `Lowest temperature : ${data['forecast'][day]['tmin']}°C`;
-            weatherTmax.innerHTML = `Highest temperature : ${data['forecast'][day]['tmax']}°C`;
-            weatherPrain.innerHTML = `Rain probability : ${data['forecast'][day]['probarain']}%`;
-            weatherSunHours.innerHTML = `Daily sun hours : ${data['forecast'][day]['sun_hours']}h`;
-            placeholder.appendChild(weatherTmax);
-            placeholder.appendChild(weatherTmin);
-            placeholder.appendChild(weatherPrain);
-            placeholder.appendChild(weatherSunHours);
-    })
-        .catch(error => {
-            console.error('Error during API request:', error);
-        })
+            try {
+                codeInsee = data[cityN]['code'];
+            }
+            catch {
+                inputError.textContent = "The post code entered is unknown";
+                return;
+            }
+            fetch(`https://api.meteo-concept.com/api/forecast/daily?token=9fc5110929e9db4b61fcc700441c5d39e82c9e6d6aeeacc3223498621f238c38&insee=${codeInsee}`)
+                .then(response => response.json())
+                .then(data => {
+                    let weatherTmin = document.createElement('div');
+                    let weatherTmax = document.createElement('div');
+                    let weatherPrain = document.createElement('div');
+                    let weatherSunHours = document.createElement('div');
+                    // Ajouter du contenu aux divs
+                    weatherTmin.innerHTML = `Lowest temperature : ${data['forecast'][day]['tmin']}°C`;
+                    weatherTmax.innerHTML = `Highest temperature : ${data['forecast'][day]['tmax']}°C`;
+                    weatherPrain.innerHTML = `Rain probability : ${data['forecast'][day]['probarain']}%`;
+                    weatherSunHours.innerHTML = `Daily sun hours : ${data['forecast'][day]['sun_hours']}h`;
+                    placeholder.appendChild(weatherTmax);
+                    placeholder.appendChild(weatherTmin);
+                    placeholder.appendChild(weatherPrain);
+                    placeholder.appendChild(weatherSunHours);
 
-    pc_submit.style.display = 'none';
-    newSearch.style.display = 'block';
-    })
+
+                    divCheckboxes = document.createElement('div');
+
+                    let checkboxTMax = document.createElement('input');
+                    checkboxTMax.type = "checkbox";
+                    checkboxTMax.id = "TMax";
+                    checkboxTMax.checked = true;
+                    let labelTMax = document.createElement('label');
+                    labelTMax.htmlFor = "TMax";
+                    labelTMax.appendChild(document.createTextNode("Highest temperature : "));
+                    divCheckboxes.appendChild(labelTMax);
+                    divCheckboxes.appendChild(checkboxTMax);
+
+                    let checkboxTMin = document.createElement('input');
+                    checkboxTMin.type = "checkbox";
+                    checkboxTMin.id = "TMin";
+                    checkboxTMin.checked = true;
+                    let labelTMin = document.createElement('label');
+                    labelTMin.htmlFor = "TMin";
+                    labelTMin.appendChild(document.createTextNode("Lowest temperature : "));
+                    divCheckboxes.appendChild(labelTMin);
+                    divCheckboxes.appendChild(checkboxTMin);
+
+                    let checkboxRainProb= document.createElement('input');
+                    checkboxRainProb.type = "checkbox";
+                    checkboxRainProb.id = "RainProb";
+                    checkboxRainProb.checked = true;
+                    let labelRainProb = document.createElement('label');
+                    labelRainProb.htmlFor = "RainProb";
+                    labelRainProb.appendChild(document.createTextNode("Rain probability : "));
+                    divCheckboxes.appendChild(labelRainProb);
+                    divCheckboxes.appendChild(checkboxRainProb);
+
+                    let checkboxSunHours = document.createElement('input');
+                    checkboxSunHours.type = "checkbox";
+                    checkboxSunHours.id = "SunHours";
+                    checkboxSunHours.checked = true;
+                    let labelSunHours = document.createElement('label');
+                    labelSunHours.htmlFor = "SunHours";
+                    labelSunHours.appendChild(document.createTextNode("Daily Sun Hours : "));
+                    divCheckboxes.appendChild(labelSunHours);
+                    divCheckboxes.appendChild(checkboxSunHours);
+
+                    let checkboxLatitude = document.createElement('input');
+                    checkboxLatitude.type = "checkbox";
+                    checkboxLatitude.id = "Latitude";
+                    let labelLatitude = document.createElement('label');
+                    labelLatitude.htmlFor = "Latitude";
+                    labelLatitude.appendChild(document.createTextNode("Latitude : "));
+                    divCheckboxes.appendChild(labelLatitude);
+                    divCheckboxes.appendChild(checkboxLatitude);                    
+                    let checkboxLongitude = document.createElement('input');
+                    checkboxLongitude.type = "checkbox";
+                    checkboxLongitude.id = "Longitude";
+                    let labelLongitude = document.createElement('label');
+                    labelLongitude.htmlFor = "Longitude";
+                    labelLongitude.appendChild(document.createTextNode("Longitude : "));
+                    divCheckboxes.appendChild(labelLongitude);
+                    divCheckboxes.appendChild(checkboxLongitude);
+                    let checkboxCumulatedRain = document.createElement('input');
+                    checkboxCumulatedRain.type = "checkbox";
+                    checkboxCumulatedRain.id = "CumulatedRain";
+                    let labelCumulatedRain = document.createElement('label');
+                    labelCumulatedRain.htmlFor = "CumulatedRain";
+                    labelCumulatedRain.appendChild(document.createTextNode("Cumulated rain in mm : "));
+                    divCheckboxes.appendChild(labelCumulatedRain);
+                    divCheckboxes.appendChild(checkboxCumulatedRain);
+
+                    let checkboxAverageWindSpeed = document.createElement('input');
+                    checkboxAverageWindSpeed.type = "checkbox";
+                    checkboxAverageWindSpeed.id = "AverageWindSpeed";
+                    let labelAverageWindSpeed = document.createElement('label');
+                    labelAverageWindSpeed.htmlFor = "AverageWindSpeed";
+                    labelAverageWindSpeed.appendChild(document.createTextNode("Average wind speed in Km/H : "));
+                    divCheckboxes.appendChild(labelAverageWindSpeed);
+                    divCheckboxes.appendChild(checkboxAverageWindSpeed);
+
+                    let checkboxWindDirection = document.createElement('input');
+                    checkboxWindDirection.type = "checkbox";
+                    checkboxWindDirection.id = "WindDirection";
+                    let labelWindDirection = document.createElement('label');
+                    labelWindDirection.htmlFor = "WindDirection";
+                    labelWindDirection.appendChild(document.createTextNode("Wind direction :  : "));
+                    divCheckboxes.appendChild(labelWindDirection);
+                    divCheckboxes.appendChild(checkboxWindDirection);                   
+                    
+                    placeholder.appendChild(divCheckboxes);
+                })
+                .catch(error => {
+                    console.error('Error during API request:', error);
+                })
+
+            pc_submit.style.display = 'none';
+            newSearch.style.display = 'block';
+        })
 })
 
 function loading() {
@@ -107,4 +195,4 @@ newSearch.addEventListener("click", () => {
     location.reload();
 });
 
-document.addEventListener("DOMContentLoaded",loading);
+document.addEventListener("DOMContentLoaded", loading);
