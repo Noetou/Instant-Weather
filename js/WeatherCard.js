1d0fdbe97eb56eb29c0f96dee82bfefc970d5c0c
TimAlbanUrl <mballu100@gmail.com>
2023-09-26 10:37:35 +0200

    feat: added label for each stat

--- js/WeatherCard.js
+++ js/WeatherCard.js
@@ -5,9 +5,11 @@ class WeatherCard{
         this.tMin=data['forecast'][days]['tmin'];
         this.latitude=data['forecast'][days]['latitude'];
         this.longitude=data['forecast'][days]['longitude'];
-        this.cumulation=data['forecast'][days]['latitude'];
+        this.cumulation=data['forecast'][days]['rr10'];
         this.windSpeed=data['forecast'][days]['wind10m'];
         this.windDirection=data['forecast'][days]['dirwind10m'];
+
+        this.labels = ['Highest temperature', 'Lowest temperature', 'Latitude', 'Longitude', 'Total rain over the day', 'Wind speed', 'Wind direction'];
 
         this.tab = new Array();
         this.tab.push(this.tMax);
@@ -20,13 +22,15 @@ class WeatherCard{
     };
     displayCard(){
         const placeholder = document.getElementById('placeholder');
+        let index = 0;
         this.tab.forEach(element => {
             if(element != null ){
                 let newDiv = document.createElement("div");
                 newDiv.classList.add("card");
-                newDiv.innerHTML = `${element}`;
+                newDiv.innerHTML = `${this.labels[index]} : ${element}`;
                 placeholder.appendChild(newDiv);
             }
+            index++;
         });
     }
 }
