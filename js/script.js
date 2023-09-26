dfb1497d264f5619edf0e59bebac42cd28a4c343
Noe Tourbillon <noe.tourbillon@etu.unicaen.fr>
2023-09-26 10:09:19 +0200

    Fix: Conflicts handling

--- js/script.js
+++ js/script.js
@@ -60,119 +60,107 @@ pc_input.addEventListener('input', () => {
     }
 });
 
-pc_submit.addEventListener('click', () => {
+pc_submit.addEventListener('click', () => { 
     pc = valueInput;
     pc_input.disabled = true;
 
     fetch('https://geo.api.gouv.fr/communes?codePostal='.concat(pc))
         .then(response => response.json())
         .then(data => {
-            let weatherTmin = document.createElement('div');
-            let weatherTmax = document.createElement('div');
-            let weatherPrain = document.createElement('div');
-            let weatherSunHours = document.createElement('div');
-            // Ajouter du contenu aux divs
-            weatherTmin.innerHTML = `Lowest temperature : ${data['forecast'][days]['tmin']}°C`;
-            weatherTmax.innerHTML = `Highest temperature : ${data['forecast'][days]['tmax']}°C`;
-            weatherPrain.innerHTML = `Rain probability : ${data['forecast'][days]['probarain']}%`;
-            weatherSunHours.innerHTML = `Daily sun hours : ${data['forecast'][days]['sun_hours']}h`;
-            placeholder.appendChild(weatherTmax);
-            placeholder.appendChild(weatherTmin);
-            placeholder.appendChild(weatherPrain);
-            placeholder.appendChild(weatherSunHours);
+    
             divCheckboxes = document.createElement('div');
 
-                    let checkboxTMax = document.createElement('input');
-                    checkboxTMax.type = "checkbox";
-                    checkboxTMax.id = "TMax";
-                    checkboxTMax.checked = true;
-                    let labelTMax = document.createElement('label');
-                    labelTMax.htmlFor = "TMax";
-                    labelTMax.appendChild(document.createTextNode("Highest temperature : "));
-                    divCheckboxes.appendChild(labelTMax);
-                    divCheckboxes.appendChild(checkboxTMax);
+            let checkboxTMax = document.createElement('input');
+            checkboxTMax.type = "checkbox";
+            checkboxTMax.id = "TMax";
+            checkboxTMax.checked = true;
+            let labelTMax = document.createElement('label');
+            labelTMax.htmlFor = "TMax";
+            labelTMax.appendChild(document.createTextNode("Highest temperature : "));
+            divCheckboxes.appendChild(labelTMax);
+            divCheckboxes.appendChild(checkboxTMax);
 
-                    let checkboxTMin = document.createElement('input');
-                    checkboxTMin.type = "checkbox";
-                    checkboxTMin.id = "TMin";
-                    checkboxTMin.checked = true;
-                    let labelTMin = document.createElement('label');
-                    labelTMin.htmlFor = "TMin";
-                    labelTMin.appendChild(document.createTextNode("Lowest temperature : "));
-                    divCheckboxes.appendChild(labelTMin);
-                    divCheckboxes.appendChild(checkboxTMin);
+            let checkboxTMin = document.createElement('input');
+            checkboxTMin.type = "checkbox";
+            checkboxTMin.id = "TMin";
+            checkboxTMin.checked = true;
+            let labelTMin = document.createElement('label');
+            labelTMin.htmlFor = "TMin";
+            labelTMin.appendChild(document.createTextNode("Lowest temperature : "));
+            divCheckboxes.appendChild(labelTMin);
+            divCheckboxes.appendChild(checkboxTMin);
 
-                    let checkboxRainProb= document.createElement('input');
-                    checkboxRainProb.type = "checkbox";
-                    checkboxRainProb.id = "RainProb";
-                    checkboxRainProb.checked = true;
-                    let labelRainProb = document.createElement('label');
-                    labelRainProb.htmlFor = "RainProb";
-                    labelRainProb.appendChild(document.createTextNode("Rain probability : "));
-                    divCheckboxes.appendChild(labelRainProb);
-                    divCheckboxes.appendChild(checkboxRainProb);
+            let checkboxRainProb = document.createElement('input');
+            checkboxRainProb.type = "checkbox";
+            checkboxRainProb.id = "RainProb";
+            checkboxRainProb.checked = true;
+            let labelRainProb = document.createElement('label');
+            labelRainProb.htmlFor = "RainProb";
+            labelRainProb.appendChild(document.createTextNode("Rain probability : "));
+            divCheckboxes.appendChild(labelRainProb);
+            divCheckboxes.appendChild(checkboxRainProb);
 
-                    let checkboxSunHours = document.createElement('input');
-                    checkboxSunHours.type = "checkbox";
-                    checkboxSunHours.id = "SunHours";
-                    checkboxSunHours.checked = true;
-                    let labelSunHours = document.createElement('label');
-                    labelSunHours.htmlFor = "SunHours";
-                    labelSunHours.appendChild(document.createTextNode("Daily Sun Hours : "));
-                    divCheckboxes.appendChild(labelSunHours);
-                    divCheckboxes.appendChild(checkboxSunHours);
+            let checkboxSunHours = document.createElement('input');
+            checkboxSunHours.type = "checkbox";
+            checkboxSunHours.id = "SunHours";
+            checkboxSunHours.checked = true;
+            let labelSunHours = document.createElement('label');
+            labelSunHours.htmlFor = "SunHours";
+            labelSunHours.appendChild(document.createTextNode("Daily Sun Hours : "));
+            divCheckboxes.appendChild(labelSunHours);
+            divCheckboxes.appendChild(checkboxSunHours);
 
-                    let checkboxLatitude = document.createElement('input');
-                    checkboxLatitude.type = "checkbox";
-                    checkboxLatitude.id = "Latitude";
-                    let labelLatitude = document.createElement('label');
-                    labelLatitude.htmlFor = "Latitude";
-                    labelLatitude.appendChild(document.createTextNode("Latitude : "));
-                    divCheckboxes.appendChild(labelLatitude);
-                    divCheckboxes.appendChild(checkboxLatitude);                    
-                    let checkboxLongitude = document.createElement('input');
-                    checkboxLongitude.type = "checkbox";
-                    checkboxLongitude.id = "Longitude";
-                    let labelLongitude = document.createElement('label');
-                    labelLongitude.htmlFor = "Longitude";
-                    labelLongitude.appendChild(document.createTextNode("Longitude : "));
-                    divCheckboxes.appendChild(labelLongitude);
-                    divCheckboxes.appendChild(checkboxLongitude);
-                    let checkboxCumulatedRain = document.createElement('input');
-                    checkboxCumulatedRain.type = "checkbox";
-                    checkboxCumulatedRain.id = "CumulatedRain";
-                    let labelCumulatedRain = document.createElement('label');
-                    labelCumulatedRain.htmlFor = "CumulatedRain";
-                    labelCumulatedRain.appendChild(document.createTextNode("Cumulated rain in mm : "));
-                    divCheckboxes.appendChild(labelCumulatedRain);
-                    divCheckboxes.appendChild(checkboxCumulatedRain);
+            let checkboxLatitude = document.createElement('input');
+            checkboxLatitude.type = "checkbox";
+            checkboxLatitude.id = "Latitude";
+            let labelLatitude = document.createElement('label');
+            labelLatitude.htmlFor = "Latitude";
+            labelLatitude.appendChild(document.createTextNode("Latitude : "));
+            divCheckboxes.appendChild(labelLatitude);
+            divCheckboxes.appendChild(checkboxLatitude);
+            let checkboxLongitude = document.createElement('input');
+            checkboxLongitude.type = "checkbox";
+            checkboxLongitude.id = "Longitude";
+            let labelLongitude = document.createElement('label');
+            labelLongitude.htmlFor = "Longitude";
+            labelLongitude.appendChild(document.createTextNode("Longitude : "));
+            divCheckboxes.appendChild(labelLongitude);
+            divCheckboxes.appendChild(checkboxLongitude);
+            let checkboxCumulatedRain = document.createElement('input');
+            checkboxCumulatedRain.type = "checkbox";
+            checkboxCumulatedRain.id = "CumulatedRain";
+            let labelCumulatedRain = document.createElement('label');
+            labelCumulatedRain.htmlFor = "CumulatedRain";
+            labelCumulatedRain.appendChild(document.createTextNode("Cumulated rain in mm : "));
+            divCheckboxes.appendChild(labelCumulatedRain);
+            divCheckboxes.appendChild(checkboxCumulatedRain);
 
-                    let checkboxAverageWindSpeed = document.createElement('input');
-                    checkboxAverageWindSpeed.type = "checkbox";
-                    checkboxAverageWindSpeed.id = "AverageWindSpeed";
-                    let labelAverageWindSpeed = document.createElement('label');
-                    labelAverageWindSpeed.htmlFor = "AverageWindSpeed";
-                    labelAverageWindSpeed.appendChild(document.createTextNode("Average wind speed in Km/H : "));
-                    divCheckboxes.appendChild(labelAverageWindSpeed);
-                    divCheckboxes.appendChild(checkboxAverageWindSpeed);
+            let checkboxAverageWindSpeed = document.createElement('input');
+            checkboxAverageWindSpeed.type = "checkbox";
+            checkboxAverageWindSpeed.id = "AverageWindSpeed";
+            let labelAverageWindSpeed = document.createElement('label');
+            labelAverageWindSpeed.htmlFor = "AverageWindSpeed";
+            labelAverageWindSpeed.appendChild(document.createTextNode("Average wind speed in Km/H : "));
+            divCheckboxes.appendChild(labelAverageWindSpeed);
+            divCheckboxes.appendChild(checkboxAverageWindSpeed);
 
-                    let checkboxWindDirection = document.createElement('input');
-                    checkboxWindDirection.type = "checkbox";
-                    checkboxWindDirection.id = "WindDirection";
-                    let labelWindDirection = document.createElement('label');
-                    labelWindDirection.htmlFor = "WindDirection";
-                    labelWindDirection.appendChild(document.createTextNode("Wind direction :  : "));
-                    divCheckboxes.appendChild(labelWindDirection);
-                    divCheckboxes.appendChild(checkboxWindDirection);                   
-                    
-                    placeholder.appendChild(divCheckboxes);                    
-                })
-                .catch(error => {
-                    console.error('Error during API request:', error);
-                })
+            let checkboxWindDirection = document.createElement('input');
+            checkboxWindDirection.type = "checkbox";
+            checkboxWindDirection.id = "WindDirection";
+            let labelWindDirection = document.createElement('label');
+            labelWindDirection.htmlFor = "WindDirection";
+            labelWindDirection.appendChild(document.createTextNode("Wind direction :  : "));
+            divCheckboxes.appendChild(labelWindDirection);
+            divCheckboxes.appendChild(checkboxWindDirection);
 
-            pc_submit.style.display = 'none';
-            newSearch.style.display = 'block';
+            placeholder.appendChild(divCheckboxes);
+        })
+        .catch(error => {
+            console.error('Error during API request:', error);
+        })
+
+    pc_submit.style.display = 'none';
+    newSearch.style.display = 'block';
 })
 
 
@@ -186,7 +174,7 @@ newSearch.addEventListener("click", () => {
     location.reload();
 });
 
-document.addEventListener("DOMContentLoaded",loading);
+document.addEventListener("DOMContentLoaded", loading);
 
 dayRange.addEventListener("change", () => {
     dayDisplay.innerHTML = `${vol.value}`;
